181a3e9b3a00fbceaafc5bd7bf8077125b82bc13
Kevin Adam Prasetya <72896188+kkms51@users.noreply.github.com>
2023-06-18 21:56:56 +0700

    PPB

--- app/src/main/java/com/example/materi_23/MainActivity.kt
+++ app/src/main/java/com/example/materi_23/MainActivity.kt
@@ -12,15 +12,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun openYoutube(view: View) {
+    fun openGmail(view: View) {
         val intent = Intent(Intent.ACTION_VIEW)
-        intent.data = Uri.parse("https://www.youtube.com")
+        intent.data = Uri.parse("https://www.gmail.com")
         startActivity(intent)
     }
 
-    fun openGoogle(view: View) {
+    fun openInsta(view: View) {
         val intent = Intent(Intent.ACTION_VIEW)
-        intent.data = Uri.parse("https://www.google.com")
+        intent.data = Uri.parse("https://www.instagram.com")
         startActivity(intent)
     }
 }
